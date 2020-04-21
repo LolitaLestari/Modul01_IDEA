5ba0269d2e85352d49ba239235431351b7cacf1f
LolitaLestari <lolita9a@gmail.com>
2020-04-22 00:12:01 +0800

    Project   IDEA

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Lolita ";
+        String namaBelakang = "lestari";
+        int usia = 21;
+        int targetTahunKuliah = 4;
+        double ipk = 3.9;
+        char nilaiAbjad = 'A';
+        boolean cantik = true;
+
+        System.out.println(namaDepan + namaBelakang);
+    }
+}
